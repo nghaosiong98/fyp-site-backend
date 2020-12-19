a781b041aafdd72cc3bb07c63ff604b5f64c69b1
nghaosiong98 <nghaosiong98@gmail.com>
2020-12-19 08:19:07 +0800

    update

--- run_docker_fastapi.sh
+++ run_docker_fastapi.sh
@@ -4,4 +4,4 @@ docker container rm fyp_fastapi_backend
 echo building docker image
 docker build -t fyp_fastapi ./fastapi_app
 echo running docker container
-docker run -d --name fyp_fastapi_backend -p 127.0.0.1:8080:80 fyp_fastapi
+docker run -d --name fyp_fastapi_backend -p 8080:80 fyp_fastapi
